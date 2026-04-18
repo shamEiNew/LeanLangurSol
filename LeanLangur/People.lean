416f40ba6b4a323940028dd11065c1cc9c9a1bd4
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2026-04-18 19:50:52 +0530

    Hackathon second day

--- LeanLangur/People.lean
+++ LeanLangur/People.lean
@@ -8,6 +8,8 @@ structure Person where
   age  : Nat
   deriving Repr, DecidableEq
 
+#check Person.mk
+
 def alice : Person :=
   { name := "Alice", age := 30 }
 
@@ -17,8 +19,39 @@ def alice : Person :=
 
 structure Voter extends Person where
   voterId : Nat
-  is_voting_eligible : 18 ≤ age
+  is_voting_eligible : 18 ≤ age := by grind
   deriving Repr, DecidableEq
 
 def bob : Voter :=
-  { name := "Bob", age := 25, voterId := 12345, is_voting_eligible := by simp }
+  { name := "Bob", age := 25, voterId := 12345}
+
+abbrev Even := { n : Nat // n % 2 = 0}
+
+example {α : Prop} (pf₁ pf₂ : α) : pf₁ = pf₂ := by
+  rfl
+
+#print Decidable
+
+instance : DecidableEq (Bool → Nat) := by
+  intro f g
+  if c₁ : f true = g true then
+    if c₂ : f false = g false then
+      apply isTrue
+      funext b
+      cases b <;> simp [c₁, c₂]
+    else
+      apply isFalse
+      grind
+  else
+    apply isFalse
+    grind
+
+structure Complicated where
+  n : Nat
+  f : Bool → Nat
+deriving DecidableEq
+
+example (f g : Nat → Nat): ∀ h:f = g,
+  f 0 = g 0 := by
+  intro h
+  rw [h]
